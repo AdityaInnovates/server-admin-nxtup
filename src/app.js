e54123e81808d4b3eb0b4bfb0c2cb3a70569413d
Aditya kumar <adityakr80510@gmail.com>
2025-01-14 02:25:11 +0530

    update

--- src/app.js
+++ src/app.js
@@ -1,29 +1,27 @@
-const express  = require('express')
-const app = express()
-require('dotenv').config()
-const db = require('../config/db')
-const cors = require('cors');
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const db = require("../config/db");
+const cors = require("cors");
 
 app.use(cors());
 
-app.get('/',(req,res)=>{
-    res.send("Hello!")
-})
+app.get("/", (req, res) => {
+  res.send("Hello!");
+});
 
-app.use(express.json())
+app.use(express.json());
 
-const PORT = process.env.PORT||5555
-console.log(process.env.PORT)
-app.listen(PORT,()=>{
-    console.log(`Running at ${PORT}`)
-})
-
+const PORT = process.env.PORT || 5555;
+console.log(process.env.PORT);
+app.listen(PORT, () => {
+  console.log(`Running at ${PORT}`);
+});
 
 // const clubRoutes = require('./routes/clubRoutes')
 // const eventsRoutes = require('./routes/eventsRoutes')
-const loginRoutes = require('./routes/loginRoutes')
+const loginRoutes = require("./routes/loginRoutes");
 
 // app.use('/clubs',clubRoutes)
 // app.use('/events',eventsRoutes)
-app.use('/login',loginRoutes)
-
+app.use("/login", loginRoutes);
